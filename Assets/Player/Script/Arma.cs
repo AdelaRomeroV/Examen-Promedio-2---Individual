74f667b44820448a449bb243ecbba748a8e89744
AdelaRomeroV <ajrvjarinee17@gmail.com>
2024-05-21 21:35:29 -0500

    Camara que sigue al jugador - Adela Romero
    no entendí lo de cámara que va arriba ;-;

--- Assets/Player/Script/Arma.cs
+++ Assets/Player/Script/Arma.cs
@@ -7,16 +7,29 @@ public class Arma : MonoBehaviour
     [SerializeField] private GameObject[] bullet;
     [SerializeField] private Transform shootPoint;
     [SerializeField] protected int CountBullet;
+    [SerializeField] protected float coolDawn;
+
+    private bool canShoot = true;
 
     protected virtual void GetDisparo(int bulletType)
     {
-        if (CountBullet > 0)
+        if (CountBullet > 0 && canShoot)
         {
-            GameObject obj = Instantiate(bullet[bulletType]);
-            obj.transform.position = shootPoint.position;
-            obj.GetComponent<BulletPlayer >().SetDirection(shootPoint.up);
-            CountBullet--;
-           
+            StartCoroutine(Shoot(bulletType));         
         } 
+    }
+
+    private IEnumerator Shoot(int bulletType)
+    {
+        canShoot = false;
+
+        GameObject obj = Instantiate(bullet[bulletType]);
+        obj.transform.position = shootPoint.position;
+        obj.GetComponent<BulletPlayer>().SetDirection(shootPoint.up);
+        CountBullet--;
+
+        yield return new WaitForSeconds(coolDawn);//espera del disparo
+
+        canShoot = true; //activacion del disparo
     }
 }
